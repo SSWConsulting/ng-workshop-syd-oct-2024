5360080ae5ac8a99885e70c870f55eb6bb98c011
Matt Wicks <matt.wicks@gmail.com>
2024-10-17 12:11:06 +1100

    company list - added dummy data

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fbc-company-list',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.scss'
 })
-export class CompanyListComponent {
+export class CompanyListComponent implements OnInit {
+  companies: any[] = [];
 
+  ngOnInit() {
+    this.getCompanies();
+  }
+
+  getCompanies() {
+    this.companies = [
+      { id: 1, name: 'Company 1', phone: '123-456-7890' },
+      { id: 2, name: 'Company 2', phone: '123-456-7890' },
+      { id: 3, name: 'Company 3', phone: '123-456-7890' },
+    ];
+  }
 }
