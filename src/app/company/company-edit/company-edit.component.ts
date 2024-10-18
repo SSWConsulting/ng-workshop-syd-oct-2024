b418deec6cdf9433c7749a06cace90236b751e2d
Matt Wicks <matt.wicks@gmail.com>
2024-10-18 11:06:08 +1100

    wired up add company to a template driven form but there is no validation

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Company } from '../company';
+import { CompanyService } from '../company.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fbc-company-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './company-edit.component.html',
-  styleUrl: './company-edit.component.scss'
+  styleUrl: './company-edit.component.scss',
 })
 export class CompanyEditComponent {
+  private companyService = inject(CompanyService);
+  private router = inject(Router);
+  company: Company = { id: 0, name: '', email: '', phone: '' };
 
+  saveCompany() {
+    this.companyService.addCompany(this.company).subscribe((company) => {
+      this.router.navigate(['/company/list']);
+    });
+  }
 }
