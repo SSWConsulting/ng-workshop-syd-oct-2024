a4654d8991cdbdae8aaba419694d238fb209363b
Matt Wicks <matt.wicks@gmail.com>
2024-10-18 14:35:16 +1100

    reactive forms - made phone number dynamic

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
@@ -12,12 +13,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 export type CompanyFormGroup = {
   [key in keyof Company]: FormControl<Company[key] | null>;
+} & {
+  checkPhone: FormControl<boolean | null>;
 };
 
 @Component({
   selector: 'fbc-company-edit',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, JsonPipe],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './company-edit.component.html',
   styleUrl: './company-edit.component.scss',
 })
@@ -32,10 +35,13 @@ export class CompanyEditComponent implements OnInit {
     id: new FormControl(0),
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl(''),
+    checkPhone: new FormControl(false),
+    phone: new FormControl({ value: '', disabled: true }),
   });
 
   ngOnInit(): void {
+    this.setupFormListeners();
+
     const companyId = +this.activatedRoute.snapshot.params['id'];
     this.isNewCompany = !companyId;
 
@@ -44,9 +50,26 @@ export class CompanyEditComponent implements OnInit {
     }
   }
 
+  setupFormListeners() {
+    this.companyFormGroup.controls.checkPhone.valueChanges.subscribe(
+      (checked) => {
+        const phoneControl = this.companyFormGroup.controls.phone;
+
+        if (checked) {
+          phoneControl.setValidators([Validators.required]);
+          phoneControl.enable();
+        } else {
+          phoneControl.clearValidators();
+          phoneControl.disable();
+        }
+        phoneControl.updateValueAndValidity();
+      },
+    );
+  }
+
   getCompany(companyId: number) {
     this.companyService.getCompany(companyId).subscribe((company) => {
-      this.companyFormGroup.patchValue(company);
+      this.companyFormGroup.patchValue({ ...company, checkPhone: !!company.phone });
     });
   }
 
