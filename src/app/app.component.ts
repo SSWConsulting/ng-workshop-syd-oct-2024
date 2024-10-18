3181a4d2a3327c66903a1a0fd697afb8cf3ad355
Matt Wicks <matt.wicks@gmail.com>
2024-10-18 15:14:23 +1100

    company service - implemented state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { CompanyListComponent } from "./company/company-list/company-list.component";
 import { RouterModule } from '@angular/router';
+import { CompanyService } from './company/company.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'fbc-root',
   standalone: true,
-  imports: [CompanyListComponent, RouterModule],
+  imports: [AsyncPipe, CompanyListComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'firebootcamp-crm';
+
+  companyService = inject(CompanyService);
+  companyCount$ = this.companyService.getCompanies().pipe(
+    map(companies => companies.length)
+  );
 }
--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,9 +1,9 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
 import { finalize, Observable, tap } from 'rxjs';
-import { CompanyTableComponent } from "../company-table/company-table.component";
+import { CompanyTableComponent } from '../company-table/company-table.component';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
@@ -13,26 +13,16 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.scss',
 })
-export class CompanyListComponent implements OnInit {
-  companies$!: Observable<Company[]>;
+export class CompanyListComponent {
+  private companyService = inject(CompanyService);
 
-  constructor(private companyService: CompanyService) { }
-
-  ngOnInit() {
-    this.getCompanies();
-  }
-
-  getCompanies() {
-    this.companies$ = this.companyService.getCompanies()
-      .pipe(
-        tap(companies => console.log('TAP - Component', companies)),
-        finalize(() => console.log('Finalize: Complete'))
-      );
-  }
+  companies$ = this.companyService.getCompanies().pipe(
+    tap((companies) => console.log('TAP - Component', companies)),
+    finalize(() => console.log('Finalize: Complete')),
+  );
 
   deleteCompany(companyId: number): void {
-    console.log("Delete company Component", companyId);
-    this.companyService.deleteCompany(companyId)
-      .subscribe(_ => this.getCompanies());
+    console.log('Delete company Component', companyId);
+    this.companyService.deleteCompany(companyId).subscribe();
   }
 }
